8fa9eea0889697dde1c6e388d53bfeba69d094b0
aapetukhov <andreybsongs@gmail.com>
2025-02-20 10:38:00 +0300

    remove artifacts

--- examples/example.py
+++
@@ -1,9 +0,0 @@
-from summarizer_graph import process_text
-
-result = process_text(
-    question="Купит ли Илон Маск ТикТок до 15-го апреля 2025 года?",
-    text="""WASHINGTON (Reuters) - US President Donald Trump said on Tuesday..."""
-)
-
-print(f"Суммаризация: {result.summary}")
-print(f"Вероятность события: {result.probability}")
