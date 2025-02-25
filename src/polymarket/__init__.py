e7391e5c2311eec9059fd5ba27bbd35351499aed
aapetukhov <andreybsongs@gmail.com>
2025-02-25 15:37:00 +0300

    fix gamma client

---
+++ src/polymarket/__init__.py
@@ -0,0 +1,13 @@
+from src.polymarket.gamma import GammaMarketClient
+from src.polymarket.polymarket import Polymarket
+from src.utils.objects import Market, PolymarketEvent, SimpleEvent, ClobReward, Tag
+
+__all__ = [
+    'GammaMarketClient',
+    'Polymarket',
+    'Market',
+    'PolymarketEvent',
+    'SimpleEvent',
+    'ClobReward',
+    'Tag'
+]
