fe00c3f0b11198fc560641a2f848efd99c5495cc
aapetukhov <andreybsongs@gmail.com>
2025-02-25 15:17:00 +0300

    fix polymarket class

---
+++ src/polymarket/polymarket.py
@@ -0,0 +1,40 @@
+import httpx
+from src.utils.objects import SimpleEvent
+
+
+class Polymarket:
+    def __init__(self, gamma_events_endpoint: str):
+        self.gamma_events_endpoint = gamma_events_endpoint
+
+    def get_all_events(self) -> "list[SimpleEvent]":
+        events = []
+        res = httpx.get(self.gamma_events_endpoint)
+        if res.status_code == 200:
+            print(len(res.json()))
+            for event in res.json():
+                try:
+                    print(1)
+                    event_data = self.map_api_to_event(event)
+                    events.append(SimpleEvent(**event_data))
+                except Exception as e:
+                    print(e)
+                    pass
+        return events
+
+    def map_api_to_event(self, event) -> SimpleEvent:
+        description = event.get("description", "")
+        return {
+            "id": int(event["id"]),
+            "ticker": event["ticker"],
+            "slug": event["slug"],
+            "title": event["title"],
+            "description": description,
+            "active": event["active"],
+            "closed": event["closed"],
+            "archived": event["archived"],
+            "new": event["new"],
+            "featured": event["featured"],
+            "restricted": event["restricted"],
+            "end": event["endDate"],
+            "markets": ",".join([x["id"] for x in event["markets"]]),
+        }
