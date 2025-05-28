d56aee1ce5414feaf559b1ad7fa40db587b99996
aapetukhov <andreybsongs@gmail.com>
2025-05-29 00:30:04 +0300

    change k vals for deepseek v3

--- src/reasoning_scripts/deepseek_v3.py
+++ src/reasoning_scripts/deepseek_v3.py
@@ -13,7 +13,7 @@ import requests
 load_dotenv()
 DEBUG = True
 TOP_K = 15
-K_VALUES = [3, 4]
+K_VALUES = [1, 2]
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_API_BASE_URL", "https://openrouter.ai/api/v1")
